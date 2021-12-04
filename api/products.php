972463f5405baf4f4d7f9c844159e0b4e3effa57
eric <eric.holmqvist@skola.taby.se>
2021-12-04 14:34:42 +0100

    lägg till product api

---
+++ api/products.php
@@ -0,0 +1,26 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+$sql = "SELECT * FROM `products`;";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "remmacs_tech";
+$conn = mysqli_connect($servername, $username, $password, $database);
+$result = $conn->query($sql);
+
+$products = [];
+#echo "Num rows: ". $result->num_rows;
+if (($result->num_rows) > 0) {
+    $index = 0;
+    while($row = $result->fetch_assoc()) {
+        $products[$index] = $row;
+        $products[$index]['id'] = intval($row["id"]);
+        $products[$index]['quantity'] = intval($row["quantity"]);
+        $products[$index]['price'] = doubleval($row["price"]);
+        $index++;
+	}
+}
+$conn->close();
+
+echo json_encode($products);
+?>
